8763bc65e1670d28a2fbe83c8ae3fd12751d83c7
quocthai84248424-byte <quocthai84248424@gmail.com>
2026-01-15 14:21:35 +0700

    Fix formatting and add local declarations

---
+++ BubbleGumHub_V4.lua
@@ -0,0 +1,9 @@
+Quay lại nha em😭😰
+Không được😼😾
+👩:tôi có người khác rồi😜🤫
+👦:người đó tên Huy đúm ko😏
+👩:đúng rồi sao anh biết😱😣
+🧑‍🤝‍🧑:bạn thân anh mà😈☠
+🗣:ồ bê xô bê xô ô á no quy no ồ bê sô bê sô
+local local local local local 
+#--@+@+#+#+#+#+
